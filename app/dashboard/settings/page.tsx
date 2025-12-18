a622eb1c19de2cdb289a22206ede9605aa70e67f
kylanalicia <aliciakimani.ky@gmail.com>
2025-12-18 22:26:42 +0300

    Created the settings page and added features to delete all connected repositories individually and as a group

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React from 'react'
+
+
+import { ProfileForm } from '@/module/settings/components/profile-form'
+import { RepositoryList } from '@/module/settings/components/repository-list'
+
+const SettingPage = () => {
+  return (
+    <div className='soace-y-6'>
+        <div>
+            <h1 className='text-3xl font-bold tracking-tight'>Settings</h1>
+            <p className='text-muted-foreground'>Manage your account settings and connected repositories</p>
+        </div>
+        <ProfileForm/>
+        <RepositoryList/>
+    </div>
+  )
+}
+
+export default SettingPage
