71ffc35907deb2cb63e8cd026e45811e2c3b8060
kylanalicia <aliciakimani.ky@gmail.com>
2025-12-18 20:48:26 +0300

    Created the following methods: fetchUserContributions, getGithubToken, getcontributionstats & getmonthlyactivity

---
+++ module/github/lib/github.ts
@@ -0,0 +1,138 @@
+import {Octokit} from "octokit"
+import { auth } from "@/lib/auth"
+import prisma from "@/lib/db"
+import { headers } from "next/headers"
+
+/**
+ * Getting the github access token
+ * 
+ */
+
+export const getGithubToken = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if(!session) {
+        throw new Error("Unauthorized")
+    }
+
+    const account = await prisma.account.findFirst({
+        where: {
+            userId: session.user.id,
+            providerId: "github"
+        }
+    })
+
+    if(!account?.accessToken) {
+        throw new Error("No github access token found")
+    }
+
+    return account.accessToken
+
+}
+
+export async function fetchUserContribution(token: string, username:string) {
+    const octokit = new Octokit({auth:token})
+
+    const query = `
+    query($username:String!){
+    user(login: $username) {
+        contributionsCollection {
+            contributionCalendar {
+                totalContributions
+                weeks{
+                    contributionDays{
+                        contributionCount
+                        date
+                        color
+                    }
+                }
+            }
+        }
+    }
+    }
+    `
+  
+    try {
+        const response:any = await octokit.graphql(query, {username})
+        return response.user.contributionsCollection.contributionCalendar
+    } catch (error) {
+        console.log("Error fetching contributions:", error)
+        return null;
+        
+    }
+
+}
+
+export const getRepositories = async(page: number = 1, perPage: number = 10) => {
+    const token = await getGithubToken();
+    const octokit = new Octokit({auth: token})
+
+    const {data} = await octokit.rest.repos.listForAuthenticatedUser({
+        sort: "updated",
+        direction: "desc",
+        visibility: "all",
+        per_page: perPage,
+        page: page
+
+    })
+    return data
+}
+
+export const createWebhook = async (owner: string, repo: string) => {
+    const token = await getGithubToken();
+    const octokit = new Octokit({auth: token})
+
+    const webhookUrl = `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/webhooks/github`
+
+    const {data:hooks} = await octokit.rest.repos.listWebhooks({
+        owner,
+        repo
+    })
+
+    const existingHook = hooks.find(hook=>hook.config.url === webhookUrl)
+    if (existingHook) {
+        return existingHook
+    }
+
+    const {data} = await octokit.rest.repos.createWebhook({
+        owner,
+        repo,
+        config:{
+            url: webhookUrl,
+            content_type: "json"
+        },
+        events: ["pull_request"]
+    })
+}
+
+export const deleteWebHook = async (owner: string, repo: string) => {
+    const token = await getGithubToken();
+    const octokit = new Octokit({auth: token})
+    const webhookUrl = `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/webhooks/github`
+
+    try {
+        const {data:hooks} = await octokit.rest.repos.listWebhooks({
+            owner,
+            repo
+        })
+
+        const hookToDelete = hooks.find(hook => hook.config.url === webhookUrl)
+
+        if(hookToDelete) {
+            await octokit.rest.repos.deleteWebhook({
+                owner,
+                repo,
+                hook_id: hookToDelete.id
+            })
+            return true
+        }
+        return false
+        
+        
+    } catch (error) {
+        console.log("Error deleting webhook:", error)
+        return false
+    }
+}
