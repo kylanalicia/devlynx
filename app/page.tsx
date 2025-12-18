de6de99196e98c70375a3ddd99add6bf37d3a468
kylanalicia <aliciakimani.ky@gmail.com>
2025-12-18 19:33:39 +0300

    Implemented a login page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { requireAuth } from "@/module/auth/utils/auth-utils";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  await requireAuth()
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Button>Hello DevLynx!</Button>
