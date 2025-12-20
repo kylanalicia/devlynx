8a8e2bce2a5356429a680abad957fef5935c68f1
kylanalicia <aliciakimani.ky@gmail.com>
2025-12-20 10:45:59 +0300

    Creared getReviews action & added review page on dashboard

--- module/review/actions/index.ts
+++ module/review/actions/index.ts
@@ -4,7 +4,6 @@ import prisma from "@/lib/db"
 import { auth } from "@/lib/auth"
 import { headers } from "next/headers"
 
-
 export async function getReviews() {
     const session = await auth.api.getSession({
         headers: await headers(),
